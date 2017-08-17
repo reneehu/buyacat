f35ef21ae7a0062341014f36c96bd731895b1665
reneehu <reneehu217@gmail.com>
2017-08-17 16:44:20 -0400

    full size container base on width size

--- app/index.js
+++ app/index.js
@@ -1,23 +1,42 @@
-    var type = "WebGL"
-    if(!PIXI.utils.isWebGLSupported()){
-    	type = "canvas"
-    }
-
-    PIXI.utils.sayHello(type);
-
-    //Create the renderer
-    var renderer = PIXI.autoDetectRenderer(256, 256);
-
-    //Add the canvas to the HTML document
-    document.body.appendChild(renderer.view);
-
-    //Create a container object called the `stage`
-    var stage = new PIXI.Container();
+require('./index.css')
 
 
-    //Tell the `renderer` to `render` the `stage`
-    renderer.render(stage);
-    renderer = PIXI.autoDetectRenderer(
-    	256, 256,
-    	{antialias: false, transparent: false, resolution: 1}
-    	);
+var type = "WebGL"
+if(!PIXI.utils.isWebGLSupported()){
+    type = "canvas"
+}
+
+PIXI.utils.sayHello(type);
+
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+
+//The renderer.view object is just a plain old ordinary <canvas> object
+renderer.view.style.border = "2px dashed #3FB6BF";
+renderer.backgroundColor = 0xffffff;
+
+//canvas to fill the entire window
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
